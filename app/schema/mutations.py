b9dd0e772f550fb92a1b111d09835afe279843f9
lautarodapin <lautarodapin@gmail.com>
2021-10-01 20:05:33 -0300

    added mutations

--- app/schema/mutations.py
+++ app/schema/mutations.py
@@ -8,45 +8,72 @@ from graphene.types.scalars import Int
 
 from app.models import ChatMessage, ChatRoom, User
 from app.schema.subscriptions import OnNewChatMessage
+from django.forms import ValidationError
 
-class Login(graphene.Mutation, name="LoginPayload"):  # type: ignore
+from .types import UserType
+
+
+class RegisterMutation(graphene.Mutation):
     ok = Boolean(required=True)
+    user = graphene.Field(UserType, required=True)
 
     class Arguments:
-        """Login request arguments."""
+        username = String(required=True)
+        password = String(required=True)
+        password2 = String(required=True)
 
+    @staticmethod
+    def mutate(self, info, username, password, password2):
+        if password != password2:
+            raise ValidationError({'password': 'Passwords are not equal'})
+        if User.objects.filter(username__icontains=username).exists():
+            raise ValidationError({'username': 'Username already exists'})
+        user = User.objects.create_user(username=username, password=password)
+        LoginMutation.mutate(self, info, username, password)
+        return RegisterMutation(ok=True, user=user)
+
+
+class LoginMutation(graphene.Mutation):
+    ok = Boolean(required=True)
+    user = graphene.Field(UserType, required=True)
+
+    class Arguments:
         username = String(required=True)
         password = String(required=True)
 
+    @staticmethod
     def mutate(self, info, username, password):
         user = authenticate(username=username, password=password)
         if user is None:
-            return Login(ok=False)
+            raise ValidationError({'__all__': 'User doesn\'t exists'})
         info.context.scope["session"] = info.context.session
         async_to_sync(login)(info.context.scope, user)
         info.context.session.save()
-        return Login(ok=True)
+        return LoginMutation(ok=True, user=user)
 
-class Logout(graphene.Mutation):
+
+class LogoutMutation(graphene.Mutation):
     ok = Boolean(required=True)
 
+    @staticmethod
     def mutate(self, info):
         user: User = info.context.user
         if user.is_authenticated:
             info.context.scope["session"] = info.context.session
             async_to_sync(logout)(info.context.scope)
             info.context.session.save()
-            return Logout(ok=True)
-        return Logout(ok=False)
+            return LogoutMutation(ok=True)
+        return LogoutMutation(ok=False)
 
 
-class SendChatMessage(graphene.Mutation, name="SendChatMessagePayload"):
+class SendChatMessageMutation(graphene.Mutation, name="SendChatMessagePayload"):
     ok = Boolean()
 
     class Arguments:
         chat_room = Int()
         message = String()
 
+    @staticmethod
     def mutate(self, info, chat_room, message):
         # Use the username from the connection scope if authorized.
         # username = (
@@ -56,16 +83,17 @@ class SendChatMessage(graphene.Mutation, name="SendChatMessagePayload"):
         # )
         user : User = info.context.user
         if not user.is_authenticated:
-            return SendChatMessage(ok=False)
+            return SendChatMessageMutation(ok=False)
         chat_room: ChatRoom = ChatRoom.objects.get(id=chat_room)
         message : ChatMessage = ChatMessage.objects.create(user=user, chat=chat_room, message=message, created_by=user, mod_by=user)
        # Notify subscribers.
         OnNewChatMessage.new_chat_message(chat_room=chat_room, message=message, sender=user)
 
-        return SendChatMessage(ok=True)
+        return SendChatMessageMutation(ok=True)
 
 
 class Mutation(ObjectType):
-    send_chat_message = SendChatMessage.Field()
-    login = Login.Field()
-    logout = Logout.Field()
+    send_chat_message = SendChatMessageMutation.Field()
+    login = LoginMutation.Field()
+    logout = LogoutMutation.Field()
+    register = RegisterMutation.Field()
--- app/schema/queries.py
+++ app/schema/queries.py
@@ -1,30 +1,34 @@
 from graphene.types.objecttype import ObjectType
 from graphene import String, List, Field
 from graphene.types.structures import NonNull
-from app.models import ChatMessage, ChatRoom
+from app.models import ChatMessage, ChatRoom, User
 
 from app.schema.types import ChatMessageType, ChatRoomType, UserType
 
 class Query(ObjectType):
-    """Root GraphQL query."""
-
     hello = String()
     history = List(NonNull(ChatMessageType), chat_room=String())
     user = Field(UserType)
-    chats = List(ChatRoomType)
+    users = Field(NonNull(UserType))
+    chat = Field(ChatRoomType, id=String(required=True))
+    chats = List(NonNull(ChatRoomType))
 
     def resolve_hello(self, info):
         return 'world'
 
     def resolve_history(self, info, chat_room: str):
-        """Return chat history."""
         return ChatMessage.objects.filter(chat=chat_room)
 
     def resolve_user(self, info):
-        """Provide currently logged in user."""
         if info.context.user.is_authenticated:
             return info.context.user
         return None
 
+    def resolve_chat(self, info, id):
+        return ChatRoom.objects.get(id=id)
+
     def resolve_chats(self, info):
         return ChatRoom.objects.all()
+
+    def resolve_users(self, info):
+        return User.objects.exclude(id=info.context.user.id)
