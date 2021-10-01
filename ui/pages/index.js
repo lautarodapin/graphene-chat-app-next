e65dc4e8b647611d6e3bbdf02651025d38a05f93
lautarodapin <lautarodapin@gmail.com>
2021-10-01 20:05:23 -0300

    Init next

---
+++ ui/pages/index.js
@@ -0,0 +1,12 @@
+import Container from "@mui/material/Container";
+
+const Homepage = () => {
+  return (
+    <Container maxWidth="sm">
+    <h1>Home Page</h1>
+    <p>lorem*15</p>
+    </Container>
+  );
+};
+
+export default Homepage;
