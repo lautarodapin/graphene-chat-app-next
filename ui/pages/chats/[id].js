26c14e2cb059f87ad6781d3c3f5a80cbd4ae1e07
lautarodapin <lautarodapin@gmail.com>
2021-10-05 18:24:36 -0300

    Basic chat app

--- ui/pages/chats/[id].js
+++ ui/pages/chats/[id].js
@@ -20,10 +20,9 @@ const ChatDetail = ({ props }) => {
     const { user } = useUser()
     const [page, setPage] = useState(1)
     const [pageSize, setPageSize] = useState(10)
-    const { data, loading, error, refetch, client} = useQuery(HISTORY, { variables: { chatRoom: id, filters: {page, pageSize} } })
+    const { data, loading, error, refetch, client } = useQuery(HISTORY, { variables: { chatRoom: id, filters: { page, pageSize } } })
     const [sendMessage] = useMutation(SEND_CHAT_MESSAGE)
     const [messages, setMessages] = useState([])
-    const [count, setCount] = useState(0)
     const [hasMore, setHasMore] = useState(false)
     const chatRef = useRef()
     const chatTopRef = useRef()
@@ -32,7 +31,7 @@ const ChatDetail = ({ props }) => {
 
     useEffect(() => {
         if (chatRef.current) {
-            chatRef.current.scrollIntoView({behavior: "smooth"})
+            chatRef.current.scrollIntoView({ behavior: "smooth" })
         }
     }, [messages])
 
@@ -40,13 +39,26 @@ const ChatDetail = ({ props }) => {
         variables: { chatRoom: id },
     })
 
+    const filterMessages = (list) => list.filter(message => !messages.some(innerMessage => innerMessage.id === message.id)) || []
+
     useEffect(() => {
-        if (data?.history) {
-            setMessages(curr => [...curr, ...data?.history.items])
-            setHasMore(data?.history.hasMore)
-            setCount(data?.history.count)
+        const firstLoad = async () => {
+            if (client && HISTORY) {
+                const { data } = await client.query({
+                    query: HISTORY, variables: {
+                        chatRoom: id, filters: { page, pageSize },
+                    }
+                })
+                console.log('load more', data)
+                if (data) {
+                    setMessages(curr => [...curr, ...filterMessages(data?.history.items)])
+                    setHasMore(data?.history.hasMore)
+                    chatRef.current.scrollIntoView()
+                }
+            }
         }
-    }, [data, setMessages])
+        firstLoad()
+    }, [])
 
     useEffect(() => {
         if (newData?.onNewChatMessage) {
@@ -57,14 +69,16 @@ const ChatDetail = ({ props }) => {
     useEffect(() => {
         const loadMore = async () => {
             if (client && HISTORY) {
-                const {data} = await client.query({query: HISTORY, variables: {
-                    chatRoom: id, filters: {page: page + 1, pageSize},
-                }})
+                const { data } = await client.query({
+                    query: HISTORY, variables: {
+                        chatRoom: id, filters: { page: page + 1, pageSize },
+                    }
+                })
                 console.log('load more', data)
                 if (data) {
-                    setMessages(curr => [...data?.history.items, ...curr])
-            chatRef.current.scrollIntoView()
-
+                    setMessages(curr => [...filterMessages(data?.history.items), ...curr])
+                    setHasMore(data?.history.hasMore)
+                    chatTopRef.current.scrollIntoView()
                 }
             }
         }
@@ -80,8 +94,6 @@ const ChatDetail = ({ props }) => {
 
     return (
         <Grid container xs={12} spacing={2}>
-            {hasMore}
-            {count}
             <Grid item xs={12}>
                 <List style={{ maxHeight: '80vh', height: '80vh', overflowY: 'auto' }}>
                     <ListItem style={{ float: "left", clear: "both" }} ref={chatTopRef}></ListItem>
@@ -112,7 +124,7 @@ const ChatDetail = ({ props }) => {
                     initialValues={{ chatRoom: id, message: '' }}
                     onSubmit={async (values, actions) => {
                         actions.setSubmitting(true)
-                        const { data } = await sendMessage({ variables: values })
+                        await sendMessage({ variables: values })
                         console.log('enviado')
                         actions.resetForm()
                         actions.setSubmitting(false)
