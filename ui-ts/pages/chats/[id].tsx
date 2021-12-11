c91d601d1e3e761ef6cf685fde2473edf9506ce3
lautarodapin <lautarodapin@gmail.com>
2021-12-11 16:26:50 -0300

    Format chat id

--- ui-ts/pages/chats/[id].tsx
+++ ui-ts/pages/chats/[id].tsx
@@ -1,27 +1,47 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import { useQuery, useMutation, useSubscription, NetworkStatus } from "@apollo/client"
 import { useRouter } from "next/dist/client/router"
-import { CircularProgress, Grid, List, ListItem, ListItemText, Paper } from "@mui/material"
+import { CircularProgress, Grid, List, ListItem, ListItemText, Paper, Skeleton } from "@mui/material"
 import { ChatInput } from "../../components/chat/chat-input"
 import { MessageList } from "../../components/chat/message-list"
-import { ScrollTo } from "../../components/scroll-to"
 import { MessageFragment, OnNewChatMessageDocument, useHistoryLazyQuery, useHistoryQuery, useJoinChatMutation, useOnNewChatMessageSubscription } from "../../generated/graphql"
 import { NextPage } from "next"
 import { useOnScreen } from "../../hooks/use-on-screen"
 import { useIsMounted } from "../../hooks/use-is-mounted"
-import { InitialMessageList } from "../../components/chat/initial-message-list"
+
 
 const ChatDetail: NextPage = () => {
     const router = useRouter()
     const id = router.query.id as string
-    const [page, setPage] = useState(1)
-    const [pageSize, setPageSize] = useState(10)
-    const { data, fetchMore, loading, error, refetch, networkStatus } = useHistoryQuery({
-        variables: { chatRoom: id, filters: { page, pageSize } },
+    const page = useRef(1)
+    const pageSize = useRef(10)
+    const initState = useRef(true)
+    const hasMore = useRef(false)
+    const [getHistory, { data, fetchMore, loading, error, refetch, networkStatus }] = useHistoryLazyQuery({
+        variables: { chatRoom: id, filters: { page: page.current, pageSize: pageSize.current } },
+        fetchPolicy: 'network-only',
+        onCompleted: (data) => {
+            if (!!data.history?.items.length) {
+                setFilterMessages(data.history?.items)
+                hasMore.current = data.history?.hasMore
+                initState.current = false
+                page.current++
+                if (chatTopRef.current) observer.observe(chatTopRef.current)
+            }
+        }
     })
-    const { data: newMessage } = useOnNewChatMessageSubscription({ variables: { chatRoom: id } })
+    useOnNewChatMessageSubscription({
+        variables: { chatRoom: id },
+        fetchPolicy: 'network-only',
+        onSubscriptionData: ({ subscriptionData }) => {
+            if (subscriptionData.data?.onNewChatMessage) {
+                console.log('new message entering', subscriptionData.data?.onNewChatMessage)
+                setFilterMessages([subscriptionData.data?.onNewChatMessage.message])
+                if (!shouldLoadMore) process.nextTick(() => chatBottomRef.current?.scrollIntoView({ behavior: 'smooth' }))
+            }
+        }
+    })
     const [joinChat] = useJoinChatMutation()
-    const [hasMore, setHasMore] = useState(false)
     const chatBottomRef = useRef<HTMLLIElement | null>(null)
     const chatTopRef = useRef<HTMLLIElement | null>(null)
     const topIsVisible = useOnScreen(chatTopRef)
@@ -32,7 +52,16 @@ const ChatDetail: NextPage = () => {
     // const isMounted = useIsMounted()
     const [messages, setMessages] = useState<MessageFragment[]>([])
 
-    const setFilterMessages = (items: MessageFragment[]) => {
+    const observer = useMemo(() => new IntersectionObserver(
+        async ([entry]) => {
+            if (entry.isIntersecting && hasMore.current && shouldLoadMore && !loading && !initState.current) {
+                observer.disconnect()
+                await refetch({ chatRoom: id, filters: { page: page.current, pageSize: pageSize.current } })
+            }
+        },
+    ), [refetch, id, loading, shouldLoadMore])
+
+    const setFilterMessages = useCallback((items: MessageFragment[]) => {
         const newMessages = [
             ...messages,
             ...items,
@@ -41,48 +70,37 @@ const ChatDetail: NextPage = () => {
                 return acc.find(m => m.id === curr.id) ? acc : [...acc, curr]
             }, [])
         setMessages(newMessages)
-    }
+        process.nextTick(() => {
+            if (!shouldLoadMore) chatBottomRef.current?.scrollIntoView({ behavior: 'smooth' })
+        })
+    }, [messages, shouldLoadMore])
 
     useEffect(() => {
-        if (newMessage?.onNewChatMessage) {
-            console.log("new message", newMessage.onNewChatMessage)
-            setFilterMessages([newMessage.onNewChatMessage.message])
-            if (!shouldLoadMore) process.nextTick(() => chatBottomRef.current?.scrollIntoView({ behavior: "smooth" }))
-        }
-    }, [newMessage])
+        getHistory({ variables: { chatRoom: id, filters: { page: 1, pageSize: pageSize.current } } })
+    }, [getHistory, id, pageSize])
 
 
-    /* TODO: initial scroll to bpotoom not working*/
-    useEffect(() => {
-        if (shouldLoadMore && !loading && hasMore && firstMount.current === false) {
-            refetch({ chatRoom: id, filters: { page: page + 1, pageSize } })
-            setPage(page + 1)
-        }
-    }, [shouldLoadMore])
 
     useEffect(() => {
-        if (data?.history) {
-            setHasMore(data.history.hasMore)
-            if (data?.history?.items.length !== 0) {
-                if (firstMount.current) firstMount.current = false
-                setFilterMessages(data.history.items)
-                if (firstMount.current) {
-                    process.nextTick(() => chatBottomRef.current?.scrollIntoView({ behavior: "smooth" }))
-                }
-            }
+        if (chatTopRef.current) {
+            observer.observe(chatTopRef.current)
         }
-        if (!shouldLoadMore) chatBottomRef.current?.scrollIntoView({ behavior: "smooth" })
-        if (shouldLoadMore) chatTopRef.current?.scrollIntoView({ behavior: "smooth" })
-    }, [data, shouldLoadMore])
-
-
-
-    if (loading) return <CircularProgress />
+        return () => { observer.disconnect() }
+    }, [chatTopRef, observer])
 
     return (
         <Grid container xs={12} spacing={2}>
             <Grid item xs={12}>
-                <List style={{ maxHeight: '80vh', height: '80vh', overflowY: 'auto' }}>
+                <List style={{ maxHeight: '80vh', height: '80vh', minHeight: '80vh', overflowY: 'auto' }}>
+                    {loading && Array.from(Array(50), (_, i) => i + 1).map(i => (
+                        <ListItem key={i}>
+                            <Grid container={true} justifyContent={i % 2 === 0 ? 'flex-start' : 'flex-end'}>
+                                <Grid item={true}>
+                                    <Skeleton animation='wave' variant='rectangular' width={350} height={75} />
+                                </Grid>
+                            </Grid>
+                        </ListItem>
+                    ))}
                     <ListItem style={{ float: "left", clear: "both" }} ref={chatTopRef}></ListItem>
                     <MessageList messages={messages} />
                     <ListItem style={{ float: "left", clear: "both" }} ref={chatBottomRef}></ListItem>
@@ -91,7 +109,7 @@ const ChatDetail: NextPage = () => {
             <Grid item xs={12}>
                 <ChatInput />
             </Grid>
-        </Grid>
+        </Grid >
     )
 }
 
